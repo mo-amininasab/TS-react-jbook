304ac1a9ab75638c216a17251d1b0cbee82c12b0
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-09-29 13:15:08 +0330

    code transpiling works

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,50 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDom from 'react-dom';
 
+import * as esbuild from 'esbuild-wasm';
+
 const App = () => {
+  const ref = useRef<any>();
+
   const [input, setInput] = useState<string>('');
   const [code, setCode] = useState<string>('');
 
-  const onClick = () => {
-    console.log(input);
-    
-  }
-  
-  return <div>
-    <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  };
+
+  return (
     <div>
-      <button onClick={onClick}>Submit</button>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
-    <pre>{code}</pre>
-  </div>;
+  );
 };
 
 ReactDom.render(<App />, document.getElementById('root'));
